2c68ac8ae806df9311f44d0447d8cb64c301f02b
Andres Lemus <aclmadrid04@gmail.com>
2023-01-13 15:28:11 -0500

    feat: setup style guide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
+const colors = require("tailwindcss/colors");
+const { fontWeight, fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Urbanist", ...fontFamily.sans],
+    },
+    fontWeight: {
+      semibold: fontWeight.semibold,
+      normal: fontWeight.normal,
+    },
+    colors: {
+      // Primary / Neutrals
+      slate: colors.slate,
+      // Supporting
+      lime: colors.lime,
+      white: colors.white,
+      transparent: colors.transparent,
+      inherit: colors.inherit,
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
